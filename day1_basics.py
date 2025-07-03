f9ba240e5fe1a1d602d380107e4745f0f26f758c
guillaume strohecker <guillaume@localhost.localdomain>
2025-07-03 17:50:06 +0100

    Day 1: Python basics and greeter mini-project

---
+++ day1_basics.py
@@ -0,0 +1,21 @@
+# Day 1 : Python basics
+
+# Variable and data types
+
+name = "Alice"
+age = 30
+height = 1.65
+is_student = True
+
+# User input
+
+user_name = input("what is your name? ")
+print("Nice to meet you", user_name)
+
+# Simple math
+
+a = 5
+b = 3
+print("Sum:", a + b)
+print("product:", a * b)
+print("Division:", a / b)
