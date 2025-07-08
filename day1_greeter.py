b22b1cec3c34a67fab48fbb2a9197c33b14ffd10
guillaume strohecker <guillaume@localhost.localdomain>
2025-07-08 15:37:14 +0100

    Remove duplicate files after organizing into folders

--- day1_greeter.py
+++
@@ -1,6 +0,0 @@
-# Personalized Greeter
-
-name = input("Enter your name: ")
-age = input("enter your age: ")
-
-print(f"Hello, {name}! Your are {age} years old.")
--- day2_sum_of_numbers.py
+++
@@ -1,5 +0,0 @@
-total = 0
-for sum in range (1,11):
-    total += sum 
-
-print(total)
